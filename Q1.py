92478a4ba2a14e879341bd298feac9b529dca388
Niyati Jain <Jainniyati905@gmail.com>
2026-06-18 08:40:21 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,7 @@
+principal = float(input("Enter principal:"))
+rate_of_interest = float(input("Enter rate of interest:"))
+time = float(input("Enter time (in year):"))
+
+simple_interest = principal*rate_of_interest*time/100
+
+print("Simple_interest:", simple_interest)
---
+++ arithmetic.py
@@ -0,0 +1,11 @@
+# calculate some thing on the code using mathematical operation use arithmetic operator.
+
+a = 10
+b = 20
+print("Addition", a+b)
+print("Subtraction", a-b)
+print("Multiplication", a*b)
+print("Division", a/b)
+print("Floor Division", a//b)
+print("Exponentiation", a**b)
+print("Modulus", a%b)
