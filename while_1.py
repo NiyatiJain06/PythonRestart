8e67734e3b5ea86870d08eace14c978afbaa36d8
Niyati Jain <Jainniyati905@gmail.com>
2026-07-06 19:54:59 +0530

    Add files via upload

---
+++ while_1.py
@@ -0,0 +1,16 @@
+""""
+Docstring for control statements.Loops. while_1
+1-10
+x=1
+while(x<=10):
+    print(x)
+    x+=1
+
+print number between 1 and 20, only even numbers
+
+"""
+x = 1
+while (x<=20):
+    if x % 2 != 0:
+        print(x)
+    x+=1    
