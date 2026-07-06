a9caf66c03fac7373ba59a49a1b2527dc4e541ac
Niyati Jain <Jainniyati905@gmail.com>
2026-07-06 19:56:06 +0530

    Add files via upload

---
+++ while_2.py
@@ -0,0 +1,7 @@
+#print number between 1 and 20, only even numbers
+
+x = 1
+while (x<=20):
+    if x % 2 != 0:
+        print(x)
+    x+=1    
---
+++ while_3.py
@@ -0,0 +1,5 @@
+# 10-1 reverse  order
+i = 10
+while (i >= 1):
+    print(i)
+    i-=1
