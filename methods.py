6fc6d88df71530d11ac21fdcd8edf888173a3779
Niyati Jain <Jainniyati905@gmail.com>
2026-07-07 20:29:36 +0530

    Add files via upload

---
+++ methods.py
@@ -0,0 +1,13 @@
+#upper()
+Name1 = "Niyati"
+Name2 = "Sparsh"
+Name3 = " "
+Name4 = "Vinita"    
+Name5 = "DEEPAK"
+print(Name1)
+print(Name2)
+print(Name3)
+print(Name4)
+print(Name5)
+print(Name1.upper())
+print(Name5.lower())
