cc6bf29e2c5325f706a5033c177004fffd60df65
Niyati Jain <Jainniyati905@gmail.com>
2026-07-07 20:28:38 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,10 @@
+str1 = "hello"
+str2 = "Niyati"
+str3 = "How are you"
+print(str1)
+print(str2)
+print(str3)
+print(type(str1))
+print(type(str2))
+print(type(str3))
+print(len(str1))
---
+++ string_2.py
@@ -0,0 +1,18 @@
+"""food1 = "Dosa"
+food2 = "Chole bhature"
+food3 = "Pizza"
+print(food1)
+print(food2)
+print(food3)
+print(type(food2)) # find data type
+print(len(food3))
+print(food2[4]) # positive index
+print(food3[-3]) # negative index
+print(food1[0:3])
+print(food2[0:13:2])
+"""
+Name = "PYTHON"
+print(Name) 
+print(Name[::-1])
+print(Name[0::])
+print(Name[:2:])
