e90ae5c730228ae61f7e676abcaf1d23a2f72f26
Niyati Jain <Jainniyati905@gmail.com>
2026-07-08 20:39:22 +0530

    Add files via upload

---
+++ endswith.py
@@ -0,0 +1,4 @@
+# vaild in the end of string.
+
+email = "niyati@examplecom"
+print(email.endswith(".com"))
---
+++ startswith.py
@@ -0,0 +1,5 @@
+"""
+Example: its use for chechking the url is secure or not
+"""
+url = "https://google.com"
+print(url.startswith("https"))
