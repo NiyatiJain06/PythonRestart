c12b44a3daea8e6dcc1ce03017828cbcff6875ac
Niyati Jain <Jainniyati905@gmail.com>
2026-07-11 21:56:06 +0530

    Add files via upload

--- tuple.py
+++ tuple.py
@@ -1,3 +1,22 @@
-Makeup_items = ('Foundation', 'Lipstick', 'Eyeliener')
-print(Makeup_items)
-print(type(Makeup_items))
+Members = ("Deepak", "Vinita", "Niyati", "Mahi", "Sparsh")
+print(Members)
+print(type(Members))
+
+# loop
+for a in Members:
+    print(a)
+
+#length
+print(len(Members))
+
+
+# Tuple can't be change
+"""x = (10,20,30)
+x[1]=100
+print(x)"""
+
+#count
+x = (10,20,30,30,30,30,20,40,50,50)
+print(x.count(30))
+
+print(x[1])
