70c88386cc7d2ca60e24d44632164c87172bfb54
Niyati Jain <Jainniyati905@gmail.com>
2026-07-11 22:02:00 +0530

    Add files via upload

---
+++ tuple_2.py
@@ -0,0 +1,8 @@
+Skills = ("Python", "Java", "Blockchain", "C", "JS")
+print(Skills)
+print(type(Skills))
+print(Skills.count("Java"))
+
+emptyTuple = ("hi",)
+print(type(emptyTuple))
+print(emptyTuple)
