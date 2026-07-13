256dbba57ed8cab97638fac07a3b848d415a18a1
Niyati Jain <Jainniyati905@gmail.com>
2026-07-13 23:33:36 +0530

    Add files via upload

---
+++ set_method.py
@@ -0,0 +1,21 @@
+#add
+s = {1,2,3}
+s.add(4)
+print(s)
+
+"""#remove
+s.remove(2)
+print(s)"""
+
+#discard
+s.discard(2)
+print(s)
+
+
+
+
+m = {1,2,3,5}
+n= {3,4,5,5}
+print(m | n)
+print(m & n)
+print(m - n)
