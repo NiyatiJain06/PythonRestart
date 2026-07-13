98063b1a9efaf80fdff62f706956cb9301669085
Niyati Jain <Jainniyati905@gmail.com>
2026-07-13 23:34:18 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+Student = {
+    "Name":["Niyati", "Mahi", "Sparsh"],
+    "Age":[20.19,18],
+    "Course":["B.tech", "Mbbs", "B.com"]
+}
+print(Student)
+print(type(Student))
+Student["Grade"] = ["A","B","C"]
+print(Student)
+Student["Age"]=[21,19,18]
+print(Student)
