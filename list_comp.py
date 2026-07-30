6152ef9f7a9a99c74c8319af0fdd06e8383905e6
Niyati Jain <Jainniyati905@gmail.com>
2026-07-30 16:33:36 +0530

    Add files via upload

---
+++ list_comp.py
@@ -0,0 +1,16 @@
+#print a square number of a numbers 1, 2, 3, 4, 5.
+
+#Expert
+numbers = [1, 2, 3, 4, 5]
+print("Before",numbers)
+squares = [i*i for i in numbers]
+print("After",squares)
+
+#Beginner
+"""print("Before square:",numbers)
+
+for i in numbers:
+    square = i*i
+    squares.append(square)
+
+print("After sqaure:",squares)  """  
