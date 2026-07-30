0159bdb87cf712500de931b4eaa2753d584b7f13
Niyati Jain <Jainniyati905@gmail.com>
2026-07-30 16:34:13 +0530

    Add files via upload

---
+++ listComp_2.py
@@ -0,0 +1,14 @@
+# convert names to uppercase
+
+#expert
+names = ["niyati","mahi","sparsh"]
+print("Lowercase names:", names)
+result = [name.upper() for name in names]
+print("Uppercase names:",result)
+
+#begnnier
+"""print("Lowercase names:", names)
+for name in names:
+    result.append(name.upper())
+
+print("Uppercase names:",result)    """
