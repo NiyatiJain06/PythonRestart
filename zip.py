23d07c8c1851d0a6927b759d3242e5362253b699
Niyati Jain <Jainniyati905@gmail.com>
2026-07-31 20:58:25 +0530

    Add files via upload

---
+++ zip.py
@@ -0,0 +1,12 @@
+Names = ["Niyati", "Mahi", "Sparsh"]
+Marks = [80, 90, 95]
+
+#expert
+for name, mark in zip(Names, Marks):
+    print(name, mark)
+
+ 
+#beginner
+"""print("Before merge:",Names ,Marks)
+for i in range(len(Names)):
+    print("After merge:",Names[i], Marks[i])"""
