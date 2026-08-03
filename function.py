5daeb687d210eccbb6603f65c94e724d6f6526f3
Niyati Jain <Jainniyati905@gmail.com>
2026-08-03 22:44:50 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,14 @@
+"""def greet(name):
+    print("Hello", name)
+
+greet("Niyati")    
+greet("Mahi")
+greet("Sparsh")
+greet("Vinita")
+greet("Deepak")"""
+
+def square(x):
+    return(x*x)
+
+x = square(25)
+print(x)
