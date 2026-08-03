bf73872d02bb0bb518915adde5c1e68320cae31b
Niyati Jain <Jainniyati905@gmail.com>
2026-08-03 22:45:28 +0530

    Add files via upload

---
+++ fun_return.py
@@ -0,0 +1,13 @@
+""""
+Basically it is use for return a value to it function, if use return in funtion so 
+the syntax will be:
+
+variable = function()
+"""
+
+def multi(x,y):
+    print("inside the parametere:",x*y)
+    return 30
+
+result = multi(30,20)
+print(result)
