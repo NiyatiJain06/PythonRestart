f12287693c8dcf0edc5b52bc4f3e9e215f29ea46
Niyati Jain <Jainniyati905@gmail.com>
2026-08-10 22:36:30 +0530

    Add files via upload

---
+++ encapsulation.py
@@ -0,0 +1,9 @@
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance= balance
+
+account1 = BankAccount("Pappu", 10000)
+print(account1.name)
+account1.balance = 5000000   
+print(account1.balance)     
