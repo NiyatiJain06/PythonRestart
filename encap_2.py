5324ae9d619588b393a8d9a957e469baa70e1ade
Niyati Jain <Jainniyati905@gmail.com>
2026-08-11 23:46:36 +0530

    Add files via upload

--- encap_2.py
+++ encap_2.py
@@ -1,7 +1,17 @@
+"""
+Bank account managment with encapsulation
+we make balance private variable
+customer use only requrid data - modify, deposit, withdraw
+"""
+
 class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name
+        self.__balance = balance #private variable
+
     def deposit(self, amount):
         if amount > 0:
-            self.__balance += amount
+            self.__balance += amount 
 
     def withdraw(self, amount):
         if 0 < amount <= self.__balance:
@@ -16,7 +26,7 @@ class BankAccount:
 acc1 = BankAccount("Pappu", 10000)
 print("1st", acc1.get_balance())
 acc1.deposit(5000)
-acc1.__balance = 100000
+acc1.__balance = 100000 # use this line in code and run the code, code will not be change because (__balance) is private variable.
 print("After deposit 5000", acc1.get_balance())
 acc1.withdraw(2000)
 print("After withdraw 2000", acc1.get_balance())
