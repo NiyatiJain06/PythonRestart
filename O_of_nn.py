4042f4bfd94b01542037dd68555d7f353f702a58
Niyati Jain <Jainniyati905@gmail.com>
2026-09-03 23:17:14 +0530

    Add files via upload

---
+++ O_of_nn.py
@@ -0,0 +1,16 @@
+def find_duplicates(numbers):
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i != j and numbers[i] == numbers[j]:
+                return True
+
+    return False
+
+numbers = [10, 22, 35, 22, 48]
+
+result = find_duplicates(numbers)
+
+if result:
+    print("Duplicate Found!")
+else:
+    print("No Duplicate Found!")    
